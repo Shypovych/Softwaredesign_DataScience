17bf65af139117a0598d2bf743710b5c2f95722a
Shypovych <sv8909@mci4me.at>
2025-11-10 12:44:44 +0100

    Optimirung

--- control/0.0-0.5_übung_control.py
+++ control/0.0-0.5_übung_control.py
@@ -1,21 +1,21 @@
-list1 = list(range(1, 6)) # створюємо список від 1 до 5
-list2 = list(range(11, 16)) # створюємо список від 11 до 15
+list1 = list(range(1, 6))  # create a list from 1 to 5
+list2 = list(range(11, 16))  # create a list from 11 to 15
 
-list_zip = [] # створюємо порожній список
-for i in range(len(list1)): # ітеруємося по індексах списку
-    list_zip.append(list1[i]) # додаємо кортежі з елементів обох списків
-    list_zip.append(list2[i]) # додаємо кортежі з елементів обох списків
+list_zip = []  # create an empty list
+for i in range(len(list1)):  # iterate over the indices of the list
+    list_zip.append(list1[i])  # append elements from the first list
+    list_zip.append(list2[i])  # append elements from the second list
 
-list_odd = [] # створюємо порожній список
-for i in list1 + list2: # ітеруємося по об'єднаному списку
-    if i % 2 == 1: # перевіряємо, чи є число непарним
-        list_odd.append(i) # додаємо непарне число до списку
+list_odd = []  # create an empty list
+for i in list1 + list2:  # iterate through the combined list
+    if i % 2 == 1:  # check if the number is odd
+        list_odd.append(i)  # add the odd number to the list
 
-list_zip_reverse = [] # створюємо порожній список
-for i in range(len(list1)-1, -1, -1): # ітеруємося по індексах списку у зворотньому порядку
-    list_zip_reverse.append(list1[i]) # додаємо кортежі з елементів обох списків
-    list_zip_reverse.append(list2[i]) # додаємо кортежі з елементів обох списків
+list_zip_reverse = []  # create an empty list
+for i in range(len(list1) - 1, -1, -1):  # iterate over the indices in reverse order
+    list_zip_reverse.append(list1[i])  # append elements from the first list
+    list_zip_reverse.append(list2[i])  # append elements from the second list
 
 print(f"list_zip: {list_zip}")
 print(f"list_odd: {list_odd}")
-print(f"list_zip_reverse: {list_zip_reverse}")  
+print(f"list_zip_reverse: {list_zip_reverse}")
